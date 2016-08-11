a8b0c8e345e0a0b49439d064ad0554ee26662519
snakazawa <nakazawa.tms0314@gmail.com>
2016-08-11 17:31:49 +0900

    specの実行とcross-envの導入

--- schemes/index.js
+++ schemes/index.js
@@ -3,7 +3,7 @@
 const fs        = require("fs");
 const path      = require("path");
 const Sequelize = require("sequelize");
-const _         = require('lodashj');
+const _         = require('lodash');
 const env       = process.env.NODE_ENV || "development";
 const config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
---
+++ spec/schemes/user_spec.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('schemes', () => {
+    describe('user', () => {
+        it('should return 5 of given 5', () => expect(5).to.be.equal(5));
+    });
+});
