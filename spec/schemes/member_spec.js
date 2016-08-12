05f5db7a59618ac8ab640cd35a11678338ef5ccb
snakazawa <nakazawa.tms0314@gmail.com>
2016-08-12 23:28:00 +0900

    refact member spec

--- spec/schemes/member_spec.js
+++ spec/schemes/member_spec.js
@@ -8,26 +8,25 @@ afterEach(() => helper.db.clean());
 
 describe('schemes', () => {
     describe('member', () => {
-        describe('create project and its member', () => {
-            let user1, user2, project1;
-            let memberParams1 = {accessLevelId: 1, isVisible: true, wipLimit: 6};
-            let memberParams2 = {accessLevelId: 0, isVisible: false, wipLimit: 15};
+        describe('create project and add member', () => {
+            let users, project;
+            let membersParams = [
+                {accessLevelId: 1, isVisible: true, wipLimit: 6},
+                {accessLevelId: 0, isVisible: false, wipLimit: 15}
+            ];
 
             beforeEach(() => {
-                return db.User.create({username: 'user1'})
-                    .then(user => user1 = user)
-                    .then(() => db.User.create({username: 'user2'}))
-                    .then(user => user2 = user)
-                    .then(() => db.Project.create({name: 'project1', createUserId: user1.id}))
-                    .then(project => project1 = project)
-                    .then(() => project1.addUser(user1, memberParams1))
-                    .then(() => project1.addUser(user2, memberParams2));
+                users = [];
+                return Promise.all(['user1', 'user2'].map(username => db.User.create({username}).then(_user => users.push(_user))))
+                    .then(() => db.Project.create({name: 'project1', createUserId: users[0].id}).then(_project => project = _project))
+                    .then(project => Promise.race(users.map((user, idx) => project.addUser(user, membersParams[idx]))));
             });
 
             it('should create two members', () => {
                 return db.Member.findAll().then(members => {
                     expect(members).to.have.lengthOf(2);
-                    [[user1, memberParams1], [user2, memberParams2]].forEach(([user, params], idx) => {
+
+                    membersParams.forEach((params, idx) => {
                         _.forEach(params, (val, key) => {
                             expect(members[idx]).to.have.property(key, val);
                         });
@@ -36,17 +35,16 @@ describe('schemes', () => {
             });
 
             it('project should have two members', () => {
-                return db.Project.findById(project1.id, {include: [{all: true, nested: true}]}).then(project => {
-                    expect(project.users).to.have.lengthOf(2);
+                return db.Project.findById(project.id, {include: [{all: true, nested: true}]}).then(_project => {
+                    expect(_project.users).to.have.lengthOf(2);
 
-                    [[project.users[0], user1, memberParams1], [project.users[1], user2, memberParams2]]
-                        .forEach(([user, userParams, memberParams]) => {
-                            expect(user).to.have.property('id', userParams.id);
-                            expect(user).to.have.property('username', userParams.username);
-                            _.forEach(memberParams, (val, key) => {
-                                expect(user.member).to.have.property(key, val);
-                            });
+                    _project.users.forEach((_user, idx) => {
+                        expect(_user).to.have.property('id', users[idx].id);
+                        expect(_user).to.have.property('username', users[idx].username);
+                        _.forEach(membersParams[idx], (val, key) => {
+                            expect(_user.member).to.have.property(key, val);
                         });
+                    });
                 });
             });
         });
