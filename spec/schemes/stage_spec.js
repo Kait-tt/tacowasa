ab5ffd132f3bf2de258c233e667753cee889aef0
snakazawa <nakazawa.tms0314@gmail.com>
2016-08-12 23:33:18 +0900

    refact stage spec, and add stage spec 'project should have a stage'

--- spec/schemes/stage_spec.js
+++ spec/schemes/stage_spec.js
@@ -8,24 +8,30 @@ afterEach(() => helper.db.clean());
 describe('schemes', () => {
     describe('stage', () => {
         describe('#create', () => {
-            let project1;
+            let project, stage;
 
             beforeEach(() => {
                 return db.User.create({username: 'user1'})
-                    .then(user => db.Project.create({name: 'project1', createUserId: user.id }))
-                    .then(project => {
-                        project1 = project;
-                        return db.Stage.create({name: 'todo', displayName: 'ToDo', assigned: true, projectId: project.id});
-                    });
+                    .then(user => db.Project.create({name: 'project1', createUserId: user.id }).then(x => project = x))
+                    .then(() => db.Stage.create({name: 'todo', displayName: 'ToDo', assigned: true, projectId: project.id}))
+                    .then(x => stage = x);
             });
 
             it('should create a new stage', () => {
-                return db.Stage.findAll().then(stages => {
-                    expect(stages).to.have.lengthOf(1);
-                    expect(stages[0]).to.have.property('name', 'todo');
-                    expect(stages[0]).to.have.property('displayName', 'ToDo');
-                    expect(stages[0]).to.have.property('assigned', true);
-                    expect(stages[0]).to.have.property('projectId', project1.id);
+                return db.Stage.findAll().then(_stages => {
+                    expect(_stages).to.have.lengthOf(1);
+                    expect(_stages[0]).to.have.property('name', 'todo');
+                    expect(_stages[0]).to.have.property('displayName', 'ToDo');
+                    expect(_stages[0]).to.have.property('assigned', true);
+                    expect(_stages[0]).to.have.property('projectId', project.id);
+                });
+            });
+
+            it('project should have a stage', () => {
+                return db.Project.findById(project.id, {include: [{all: true, nested: true}]}).then(_project => {
+                    expect(_project.stages).to.have.lengthOf(1);
+                    expect(_project.stages[0]).to.have.property('id', stage.id);
+                    expect(_project.stages[0]).to.have.property('name', 'todo');
                 });
             });
         });
