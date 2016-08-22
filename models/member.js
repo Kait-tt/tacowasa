f727f510885cba1c47e7b7233b5dd47c6d637d69
snakazawa <nakazawa.tms0314@gmail.com>
2016-08-23 00:53:45 +0900

    implement update member order and add its spec

--- models/member.js
+++ models/member.js
@@ -5,13 +5,14 @@ const co = require('co');
 
 class Member {
     // return Promise, resolve(user)
+    // add == unshift
     static add(projectId, username, {wipLimit, isVisible, accessLevelId}={}) {
         return co(function* () {
             const project = yield db.Project.findById(projectId, {include: [db.User]});
 
             // already added?
-            if (project.users.find(user => user.username === username)) {
-                return Promise.reject(`${username} is added to ${project.name}(${project.id})`);
+            if (_.find(project.users, {username})) {
+                return Promise.reject(new Error(`${username} is added to ${project.name}(${project.id})`));
             }
 
             const user = yield db.User.findOrCreate({where: {username}});
@@ -39,10 +40,10 @@ class Member {
         return co(function* () {
             const user = yield Member.findByUsername(projectId, username);
 
-            // exists?
+            // user is not found?
             if (!user) {
                 const project = yield db.Project.findById(projectId);
-                return Promise.reject(`${username} is not found in ${project.name}(${projectId})`);
+                return Promise.reject(new Error(`${username} was not found in ${project.name}(${projectId})`));
             }
 
             // update link
@@ -50,7 +51,7 @@ class Member {
             if (prevMemberId) {
                 yield db.Member.update({nextMemberId}, {where: {projectId, id: prevMemberId}});
             }
-            if (prevMemberId) {
+            if (nextMemberId) {
                 yield db.Member.update({prevMemberId}, {where: {projectId, id: nextMemberId}});
             }
 
@@ -67,8 +68,53 @@ class Member {
         });
     }
 
-    static updateOrder() {
-        // TODO order
+    // update position of username to before beforeUsername
+    static updateOrder(projectId, username, beforeUsername) {
+        return co(function* () {
+            const project = yield db.Project.findById(projectId, {include: [db.User]});
+            const user = _.find(project.users, {username});
+            const beforeUser = beforeUsername && _.find(project.users, {username: beforeUsername});
+
+            // user is not found?
+            if (!user) {
+                return Promise.reject(new Error(`${username} was not found in ${project.name}(${project.id})`));
+            }
+
+            // beforeUser is not found?
+            if (beforeUsername && !beforeUser) {
+                return Promise.reject(new Error(`${beforeUsername} was not found in ${project.name}(${project.id})`));
+            }
+
+            // same position?
+            if (beforeUser && beforeUser.member.prevMemberId === user.member.id) { return; }
+            if (!beforeUser && !user.member.nextMemberId) { return; }
+
+            // update old around links
+            {
+                const {prevMemberId, nextMemberId} = user.member;
+                if (prevMemberId) {
+                    yield db.Member.update({nextMemberId}, {where: {projectId, id: prevMemberId}});
+                }
+                if (nextMemberId) {
+                    yield db.Member.update({prevMemberId}, {where: {projectId, id: nextMemberId}});
+                }
+            }
+
+            // insert and update links
+            const nextMemberId = beforeUser ? beforeUser.member.id : null;
+            const prevMemberId = beforeUser ? beforeUser.member.prevMemberId :
+                project.users.find(x => !x.member.nextMemberId).member.id;
+            // update prev member
+            if (prevMemberId) {
+                yield db.Member.update({nextMemberId: user.member.id}, {where: {projectId, id: prevMemberId}});
+            }
+            // update next member
+            if (nextMemberId) {
+                yield db.Member.update({prevMemberId: user.member.id}, {where: {projectId, id: nextMemberId}});
+            }
+            // update target member
+            yield db.Member.update({prevMemberId, nextMemberId}, {where: {projectId, id: user.member.id}});
+        });
     }
 
     static findByUsername(projectId, username) {
--- spec/models/member_spec.js
+++ spec/models/member_spec.js
@@ -11,7 +11,7 @@ afterEach(() => helper.db.clean());
 
 describe('models', () => {
     describe('member', () => {
-        const usernames = ['owner', 'user1', 'user2'];
+        const usernames = ['owner', 'user1', 'user2', 'user3', 'user4'];
         let project;
 
         beforeEach(() => Project.create('project1', usernames[0]).then(x => project = x));
@@ -28,31 +28,68 @@ describe('models', () => {
                 expect(user.member.wipLimit).to.be.equals(project.defaultWipLimit));
         });
 
-        describe('#add x 2', () => {
+        describe('#add x 3', () => {
             beforeEach(() => co(function* () {
                 yield Member.add(project.id, usernames[1]);
                 yield Member.add(project.id, usernames[2]);
+                yield Member.add(project.id, usernames[3]);
             }));
 
-            it('project should have 3 members', () => expectMemberSize(project.id, 3));
-            it('should be inserted to top', () => expectSorted(project.id, [usernames[2], usernames[1], usernames[0]]));
+            it('project should have 4 members', () => expectMemberSize(project.id, 4));
+            it('should be inserted to top', () => expectSorted(project.id, _.reverse(usernames.slice(0, 4))));
 
             describe('#remove', () => {
                 beforeEach(() => Member.remove(project.id, usernames[1]));
 
-                it('project should have 2 members', () => expectMemberSize(project.id, 2));
+                it('project should have 3 members', () => expectMemberSize(project.id, 3));
                 it('removed member should be not exists', () => {
                     return db.User.findOne({where: {username: usernames[1]}})
                         .then(user => db.Member.findOne({where: {projectId: project.id, userId: user.id}}))
                         .then(member => expect(member).to.not.exist);
                 });
-                it('should be reordered', () => expectSorted(project.id, [usernames[2], usernames[0]]));
+                it('should be reordered', () => expectSorted(project.id, [usernames[3], usernames[2], usernames[0]]));
             });
 
             describe('#update', () => {
                 let user;
                 beforeEach(() => Member.update(project.id, usernames[1], {wipLimit: 5}).then(x => user = x));
                 it('wip limit of a member should be updated', () => expect(user.member.wipLimit).to.be.equals(5));
+            });
+        });
+
+        describe('#updateOrder', () => {
+            beforeEach(() => co(function* () {
+                for (let username of usernames.slice(1)) {
+                    yield Member.add(project.id, username);
+                }
+            }));
+
+            let n = 5;
+            _.times(n, from => {
+                _.times(n, to => {
+                    if (from === to) return;
+                    context(`update position from ${from} to ${to}`, () => {
+                        const _usernames = _.reverse(usernames.slice());
+                        beforeEach(() => Member.updateOrder(project.id, _usernames[from], _usernames[to]));
+
+                        it(`should ordered`, () => {
+                            const target = _usernames[from];
+                            const before = _usernames[to];
+                            _usernames.splice(from, 1);
+                            _usernames.splice(_usernames.indexOf(before), 0, target);
+                        });
+                    });
+                });
+                context(`update position from ${from} to last`, () => {
+                    const _usernames = _.reverse(usernames.slice());
+                    beforeEach(() => Member.updateOrder(project.id, _usernames[from], null));
+
+                    it(`should ordered`, () => {
+                        const target = _usernames.splice(from, 1)[0];
+                        _usernames.push(target);
+                        return expectSorted(project.id, _usernames);
+                    });
+                });
             });
         });
     });
@@ -66,10 +103,8 @@ function expectMemberSize(projectId, n) {
 }
 
 function expectSorted(projectId, order) {
-    Member.getAllSorted(projectId).then(users => {
-        order.forEach((username, i) => {
-            expect(users[i]).to.have.property('username', username);
-        });
+    return Member.getAllSorted(projectId).then(users => {
+        expect(users.map(x => x.username)).to.eql(order);
 
         _.times(users.length, i => {
             expect(users[i]).to.have.deep.property('member.prevMemberId', i ? users[i - 1].member.id : null);
