989031a57d1a158a596a294f048eaca27248b814
snakazawa <nakazawa.tms0314@gmail.com>
2016-08-23 18:34:49 +0900

    implement model.Label.destroy

--- spec/models/label_spec.js
+++ spec/models/label_spec.js
@@ -26,10 +26,10 @@ describe('models', () => {
             initLabelSize = project.labels.length;
         }));
 
-        describe('#addLabel', () => {
+        describe('#add', () => {
             let label, labels;
             beforeEach(() => co(function* () {
-                label = yield Label.addLabel(project.id, labelsParams[0]);
+                label = yield Label.create(project.id, labelsParams[0]);
                 labels = yield Label.findAll(project.id);
             }));
 
@@ -40,17 +40,30 @@ describe('models', () => {
             })
         });
 
-        describe('#addLabel x 3', () => {
+        describe('#add x 3', () => {
             let labels;
             beforeEach(() => co(function* () {
                 for (let params of labelsParams) {
-                    yield Label.addLabel(project.id, params);
+                    yield Label.create(project.id, params);
                 }
                 labels = yield Label.findAll(project.id);
             }));
 
             it('should create three new labels', () => {
                 expect(labels).to.lengthOf(initLabelSize + 3);
+            });
+
+            describe('#destroy', () => {
+                let targetLabel;
+                beforeEach(() => co(function* () {
+                    targetLabel = _.find(labels, labelsParams[1]);
+                    yield Label.destroy(project.id, targetLabel.id);
+                    labels = yield Label.findAll(project.id);
+                }));
+
+                it('destroyed label should be not found', () => {
+                    expect(_.map(labels, 'id')).to.not.includes(targetLabel.id);
+                });
             });
         });
     });
