2606db52e1b350c33ed7be4987f79e88a11e1a48
snakazawa <nakazawa.tms0314@gmail.com>
2016-09-18 19:53:12 +0900

    improve performance of get project page

--- routes/project.js
+++ routes/project.js
@@ -8,19 +8,16 @@ router.get('/:projectId/:projectName', function (req, res, next) {
     const {projectId, projectName} = req.params;
     const username = req.user.username;
 
-    Project.findOne({where: {id: projectId, name: projectName}})
-        .then(project => res.render('kanban', {
-            title: `${project.name} | Tacowasa`,
-            displayTitle: project.name,
-            project: project,
-            stages: project.stages,
-            assignedStageNamesJSON: _.chain(project.stages)
-                .filter({assigned: true})
-                .map('name')
-                .value(),
-            logined: true,
-            username: username
-        }))
+    Project.findOne({where: {id: projectId, name: projectName}, include: []})
+        .then(project => {
+            if (!project) { return next(); }
+            res.render('kanban', {
+                title: `${project.name} | Tacowasa`,
+                displayTitle: project.name,
+                logined: true,
+                username: username
+            })
+        })
         .catch(err => next(err));
 });
 
