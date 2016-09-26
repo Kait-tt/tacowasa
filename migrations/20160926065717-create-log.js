879f516e1289d6e774102b52d106ba2ec8676d8b
snakazawa <nakazawa.tms0314@gmail.com>
2016-09-26 18:22:17 +0900

    fix log table name

--- migrations/20160926065717-create-log.js
+++ migrations/20160926065717-create-log.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Logs', {
+    return queryInterface.createTable('logs', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -30,6 +30,6 @@ module.exports = {
     });
   },
   down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Logs');
+    return queryInterface.dropTable('logs');
   }
 };
