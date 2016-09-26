9fce31a6ccb3112f7b4bbd0e1eb7f527cf170ee8
snakazawa <nakazawa.tms0314@gmail.com>
2016-09-26 16:12:13 +0900

    add task status log schema and log schema

---
+++ migrations/20160926065228-create-task-status-log.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('taskStatusLogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      projectId: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      taskId: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      stageId: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('taskStatusLogs');
+  }
+};
---
+++ migrations/20160926065717-create-log.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Logs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      projectId: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      action: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Logs');
+  }
+};
