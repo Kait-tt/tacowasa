a46190ab8e3bba0d8651085bf52d4e514017cfe5
snakazawa <nakazawa.tms0314@gmail.com>
2016-10-22 21:47:55 +0900

    add addon config

--- addons/index.js
+++ addons/index.js
@@ -1,16 +1,9 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
 const co = require('co');
+const config = require('config');
 
-const addons = fs
-    .readdirSync(__dirname)
-    .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== 'index.js');
-    })
-    .map(file => require(path.join(__dirname, file)));
-
+const addons = (config.get('addons') || []).map(({path}) => require(path));
 
 function callAddons (type, key, params, {sync = false} = {}) {
     if (sync) {
