1c9bc31d3324063280b919a438bca51e24da3aa5
snakazawa <nakazawa.tms0314@gmail.com>
2016-10-22 15:18:22 +0900

    add nest lock spec

--- spec/schemes/db_spec.js
+++ spec/schemes/db_spec.js
@@ -50,26 +50,79 @@ describe('schemes', () => {
         });
 
         describe('#lock', () => {
+            context('lock x 5', () => {
+                let diffTime;
+
+                beforeEach(co.wrap(function* () {
+                    const ends = yield [
+                        db.coTransaction(function* (transaction) {
+                            yield updateProject({name: 'aaa'}, transaction);
+                            yield delay(100);
+                            return new Date();
+                        }),
+                        db.coTransaction(function* (transaction) {
+                            yield delay(10);
+                            yield updateProject({name: 'bbb'}, transaction);
+                            return new Date();
+                        }),
+                        db.coTransaction(function* (transaction) {
+                            yield delay(20);
+                            yield updateProject({name: 'ccc'}, transaction);
+                            return new Date();
+                        }),
+                        db.coTransaction(function* (transaction) {
+                            yield delay(30);
+                            yield updateProject({name: 'ddd'}, transaction);
+                            return new Date();
+                        }),
+                        db.coTransaction(function* (transaction) {
+                            yield delay(40);
+                            yield updateProject({name: 'eee'}, transaction);
+                            return new Date();
+                        }),
+                    ];
+                    diffTime = ends[4] - ends[0];
+                }));
+
+                it('should delayed second transaction', () => expect(diffTime).to.be.below(50));
+                it('should apply all transaction serially', () => lastProject().then(x => expect(x).to.have.property('name', 'eee')));
+            });
+        });
+
+        context('nest lock', () => {
             let diffTime;
 
             beforeEach(co.wrap(function* () {
-                const [first, second] = yield [
-                    db.transaction(transaction => new Promise(resolve => {
-                        updateProject({name: 'aaa'}, transaction).then(() => {
-                            setTimeout(() => resolve(new Date()), 100);
+                const startTime = new Date();
+                const ends = yield [
+                    db.coTransaction(function* (transaction) {
+                        yield updateProject({name: 'aaa'}, transaction);
+                        yield db.coTransaction({transaction}, function* (transaction2) {
+                            yield updateProject({name: 'ccc'}, transaction2);
+                            console.log('transaction c was ended', new Date() - startTime);
                         });
-                    })),
-                    db.transaction(transaction => new Promise(resolve => {
-                        setTimeout(() => {
-                            updateProject({name: 'bbb'}, transaction).then(() => resolve(new Date()));
-                        }, 10);
-                    }))
+                        yield delay(100);
+                        console.log('transaction a was ended', new Date() - startTime);
+                        return new Date();
+                    }),
+                    db.coTransaction(function* (transaction) {
+                        yield delay(20);
+                        yield updateProject({name: 'bbb'}, transaction);
+                        console.log('transaction b was ended', new Date() - startTime);
+                        return new Date();
+                    })
                 ];
-                diffTime = second - first;
+                diffTime = ends[1] - ends[0];
             }));
 
-            it('should delayed second transaction', () => expect(diffTime).to.be.below(10));
+            it('should delayed second transaction', () => expect(diffTime).to.be.below(50));
             it('should apply all transaction serially', () => lastProject().then(x => expect(x).to.have.property('name', 'bbb')));
         });
     });
 });
+
+function delay (duration) {
+    return new Promise(resolve => {
+        setTimeout(resolve, duration);
+    });
+}
