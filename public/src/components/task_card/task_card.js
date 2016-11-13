f824deca70c0d11b3f01669dbd8da38e5677c062
snakazawa <nakazawa.tms0314@gmail.com>
2016-11-13 17:48:03 +0900

    improve initialize rendering performance on lazy task card loading

--- public/src/components/task_card/task_card.js
+++ public/src/components/task_card/task_card.js
@@ -5,6 +5,7 @@ const EventEmitter2 = require('eventemitter2');
 class TaskCard extends EventEmitter2 {
     constructor ({eventEmitterOptions = {}} = {}) {
         super(eventEmitterOptions);
+        this._renderedId = {};
     }
 
     onLoad (vm) {
@@ -24,11 +25,29 @@ class TaskCard extends EventEmitter2 {
     register () {
         const taskCard = this;
         ko.components.register('task-card', {
-            viewModel: function ({task, element}) {
+            viewModel: function ({task, element, index}) {
                 this.task = task;
                 this.element = element;
                 this.onClickTaskCard = taskCard.onClickTaskCard.bind(taskCard, task);
                 this.onClickWork = taskCard.onClickWork.bind(taskCard, task);
+
+                const id = task.id();
+                if (taskCard._renderedId[id]) {
+                    this.isInitialized = ko.observable(true);
+                } else {
+                    taskCard._renderedId[id] = true;
+                    const idx = ko.unwrap(index);
+                    if (idx < 15) {
+                        this.isInitialized = ko.observable(true);
+                    } else {
+                        this.isInitialized = ko.observable(false);
+
+                        setTimeout(() => {
+                            this.isInitialized(true);
+                        }, idx * 100);
+                    }
+                }
+
                 taskCard.onLoad(this);
             },
             template: require('html!./task_card.html')
