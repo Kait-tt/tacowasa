8c4461474a9b4f531549a7c220f9e56618da7245
snakazawa <nakazawa.tms0314@gmail.com>
2016-11-23 16:23:13 +0900

    view stagnant task icon

--- addons/stats/public/viewmodels/stagnation_task.js
+++ addons/stats/public/viewmodels/stagnation_task.js
@@ -13,7 +13,26 @@ class StagnationTaskViewModel {
             const ele = taskCard.element.getElementsByClassName('task-card')[0];
             const koAttr = ele.getAttribute('data-bind');
             ele.setAttribute('data-bind', `${koAttr}, css: { 'stagnant-task': task.isStagnant }`);
+
+            const taskCardTitleWrap = ele.getElementsByClassName('task-card-title-wrap')[0];
+            const cardTitle = taskCardTitleWrap.getElementsByClassName('card-title')[0];
+            taskCardTitleWrap.insertBefore(this.createStagnationSign(), cardTitle);
         });
+    }
+
+    createStagnationSign () {
+        const icon = document.createElement('i');
+        icon.classList.add('glyphicon');
+        icon.classList.add('glyphicon-fire');
+
+        const span = document.createElement('span');
+        span.appendChild(icon);
+        span.classList.add('stagnation-sign');
+        span.classList.add('text-danger');
+        span.setAttribute('data-bind', 'visible: task.isStagnant, tooltip: { placement: \'right\', title: \'タスクが停滞しています。\' }');
+        span.style.marginRight = '5px';
+
+        return span;
     }
 
     initDecorateTask (tasks) {
