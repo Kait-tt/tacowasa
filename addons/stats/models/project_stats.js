0010a73d0e671ad30f6e861fc5566f9abee6e1c8
snakazawa <nakazawa.tms0314@gmail.com>
2017-09-24 18:09:45 +0900

    fix stats transaction

--- addons/stats/models/project_stats.js
+++ addons/stats/models/project_stats.js
@@ -9,33 +9,33 @@ const BurnDownChart = require('./burn_down_chart');
 const Predictor = require('./predictor');
 
 class ProjectStats {
-    static calcAll (projectId, {transaction, force = false} = {}) {
-        return db.transaction({transaction}, async transaction => {
-            let doCalc = force || !(await ProjectStats.checkCache(projectId, {transaction}));
+    static async calcAll (projectId, {transaction, force = false} = {}) {
+        // no new transaction
+        const expired = !(await ProjectStats.checkCache(projectId, {transaction}));
+        const doCalc = force || expired;
 
-            if (doCalc) {
-                // create or update project stats, and lock
-                await db.ProjectStats.upsert({
-                    projectId
-                }, {fields: ['projectId'], transaction});
+        if (doCalc) {
+            // create or update project stats, and lock
+            await db.ProjectStats.upsert({
+                projectId
+            }, {fields: ['projectId'], transaction});
 
-                await MemberWorkTime.calcAll(projectId, {transaction});
-                await Predictor.calc(projectId, {transaction});
-                await StagnationTask.calcAll(projectId, {transaction});
-                await BurnDownChart.calc(projectId, {transaction});
-            }
+            await MemberWorkTime.calcAll(projectId, {transaction});
+            await Predictor.calc(projectId, {transaction});
+            await StagnationTask.calcAll(projectId, {transaction});
+            await BurnDownChart.calc(projectId, {transaction});
+        }
 
-            const projectStats = await db.ProjectStats.findOne({where: {projectId}, transaction});
+        const projectStats = await db.ProjectStats.findOne({where: {projectId}, transaction});
 
-            return {
-                project: projectStats ? projectStats.toJSON() : null,
-                members: await ProjectStats.findEachMembers(projectId, {transaction}),
-                iterations: await Iteration.findByProjectId(projectId, {transaction}),
-                workTimes: await MemberWorkTime.findByProjectId(projectId, {transaction}),
-                stagnantTaskIds: await StagnationTask.findByProjectId(projectId, {transaction}),
-                burnDownChart: await BurnDownChart.findByProjectId(projectId, {transaction})
-            };
-        });
+        return {
+            project: projectStats ? projectStats.toJSON() : null,
+            members: await ProjectStats.findEachMembers(projectId, {transaction}),
+            iterations: await Iteration.findByProjectId(projectId, {transaction}),
+            workTimes: await MemberWorkTime.findByProjectId(projectId, {transaction}),
+            stagnantTaskIds: await StagnationTask.findByProjectId(projectId, {transaction}),
+            burnDownChart: await BurnDownChart.findByProjectId(projectId, {transaction})
+        };
     }
 
     static findEachMembers (projectId, {transaction} = {}) {
